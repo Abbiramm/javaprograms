a7a4b5911e32cc545d1ad76f35fd1006ad5ae8bb
Abbiramm <116068702+Abbiramm@users.noreply.github.com>
2022-10-18 14:59:11 +0530

    Add files via upload

---
+++ abb1.java
@@ -0,0 +1,11 @@
+package ab1;
+
+public class abb1 {
+	public static void main(String args[])
+	{
+		System.out.println("ABBIRAM");
+		System.out.println("From karur");
+		System.out.println("Tamilnadu");
+	}
+
+}
