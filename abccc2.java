b47b8131935e30569c526e7c3c0ea15670f3b3ef
Abbiramm <116068702+Abbiramm@users.noreply.github.com>
2022-10-18 15:48:36 +0530

    Add files via upload

---
+++ abccc2.java
@@ -0,0 +1,38 @@
+package asd2;
+
+public class abccc2 {
+	public static void main(String args[])
+	{
+		int intval;
+		float fval;
+		double dval;
+		char cval;
+		short sval;
+		byte bval;
+		long lval;
+		boolean boolval;
+		
+		//Assign the values
+		intval=468;
+		
+		System.out.println("Integer value=" +intval);
+		fval=468.5f;
+		System.out.println("Float value=" +fval);
+		
+		dval=468.9;
+		cval='A';
+		lval=9871;
+		boolval=true;
+		
+		System.out.println("Double value=" +dval);
+		System.out.println("Char value=" +cval);
+		System.out.println("Long value=" +lval);
+		System.out.println("Boolean value=" +boolval);
+
+		}
+
+	}
+
+	  
+   
+
